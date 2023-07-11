ddcbb27472816e3c947a3c7dbd837846c7a9f7a0
Tiva <gyoritiv@gmail.com>
2023-07-11 15:51:12 +0200

    parameterized docker runner

--- tf_serving.ps1
+++ tf_serving.ps1
@@ -1,9 +1,6 @@
 
-$PORTNUMBER = jq '.portNumber' .\config.json
-$MODELNAME = jq '.modelName' .\config.json
-$CONTAINERNAME = jq '.containerName' .\config.json
-$MNISTPATH = "//C//Repositories//mlops_homework//mlops//mnist_model"
-echo $MNISTPATH
-echo $PORTNUMBER
-echo $CONTAINERNAME
-docker run -p 8501:8501 --name tfserving_fashion_mnist -v C:\\Repositories\\mlops_homework\\mnist_model:/models/mnist_model -e MODEL_NAME=mnist_model -t tensorflow/serving
+$PORTNUMBER = jq -r '.portNumber' .\config.json
+$MODELNAME = jq -r '.modelName' .\config.json
+$CONTAINERNAME = jq -r '.containerName' .\config.json
+$MODELFULLPATH = jq -r '.modelFullPath' .\config.json
+docker run -p "${PORTNUMBER}:${PORTNUMBER}" --name "${containerName}" -v C:\\Repositories\\mlops_homework\\mnist_model:/models/mnist_model -e MODEL_NAME="${MODELNAME}" -t tensorflow/serving
